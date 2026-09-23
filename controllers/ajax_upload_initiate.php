f56e31ba417352cb2a178009f6f63b05eb15b863
Alwen <alwencasagann@gmail.com>
2026-09-23 20:58:51 +0800

    fix: register EduPortal namespace import and pin Composer platform to PHP 8.2

--- controllers/ajax_upload_initiate.php
+++ controllers/ajax_upload_initiate.php
@@ -14,6 +14,8 @@ if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/ObjectStorageService.php';
 
+use EduPortal\ObjectStorageService;
+
 requireLogin();
 header('Content-Type: application/json');
 
